661c3717a5abc3c0777b61e16f1b5a6b40fcd48e
ttakazaki <urd59675@gmail.com>
2018-06-06 22:28:33 +0900

    react-redux-TodoApp

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 import tasksReducer from "./reducers/tasks";
-import TodoApp from './components/TodoApp'
-import { createStore } from "redux";;
+import TodoApp from './containers/TodoApp'
+import { createStore } from "redux";
+import { Provider } from 'react-redux';
 
 const store = createStore(tasksReducer);
 
-function renderApp (store) {
-  render(
-    <TodoApp store={store} />,
-    document.getElementById('root')
-  )
-}
-
-store.subscribe(() => renderApp(store));
-renderApp(store);
+render(
+  <Provider store={store}>
+    <TodoApp />
+  </Provider>,
+  document.getElementById('root')
+)
