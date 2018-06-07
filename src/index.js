761c35cb354c03a16db467c5eb86b8c69c89e075
ttakazaki <urd59675@gmail.com>
2018-06-07 21:51:08 +0900

    material-ui

--- src/index.js
+++ src/index.js
@@ -1,67 +1,15 @@
-import React from 'react'
-import {Provider} from 'react-redux'
-import {createStore, applyMiddleware} from 'redux'
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { render } from 'react-dom';
+import tasksReducer from './reducers/tasks';
+import TodoApp from './containers/TodoApp';
 
-import Todo from './components/Todo'
-import ReactDOM from 'react-dom'
+const store = createStore(tasksReducer);
 
-const initialState = {
-  task: '',
-  tasks: []
-}
-
-function tasksReducer (state = initialState, action) {
-  switch (action.type) {
-    case 'INPUT_TASK':
-      return {
-        ...state,
-        task: action.task
-      }
-    case 'ADD_TASK':
-      return {
-        ...state,
-        tasks: state.tasks.concat([action.task])
-      }
-    default:
-      return state;
-  }
-}
-
-// ログミドルウェア
-const logger = store => next => action => {
-  // Action適用前のstateを表示
-  console.log(store.getState())
-
-  // どのようなActionが適用されたのかを表示
-  console.log(action)
-
-  const result = next(action)
-
-  // Action適用後のstateを表示
-  console.log(store.getState())
-  console.log('-----------------')
-
-  // 特別なreturnする必要がないのでresultをそのまま返す
-  return result
-}
-
-const storageMiddleware = store => next => action => {
-  const result = next(action)
-  window.localStorage.setItem('app-state', JSON.stringify(store.getState()))
-  return result
-}
-
-const savedState = JSON.parse(localStorage.getItem('app-state'))
-const store = createStore(
-  tasksReducer,
-  savedState ? savedState : tasksReducer(undefined, {type: 'Init'}),
-  applyMiddleware(logger,storageMiddleware)
-)
-
-ReactDOM.render(
+render(
   <Provider store={store}>
-    <Todo />
+    <TodoApp />
   </Provider>,
   document.getElementById('root')
-)
-
+);
